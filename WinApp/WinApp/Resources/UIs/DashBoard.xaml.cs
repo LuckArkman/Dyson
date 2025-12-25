371ed20465181dd4d4d250055b684783482ddb12
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2025-12-24 22:14:17 -0300

    up

---
+++ WinApp/WinApp/Resources/UIs/DashBoard.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinApp.Resources.UIs;
+
+public partial class DashBoard : ContentPage
+{
+    public DashBoard()
+    {
+        InitializeComponent();
+    }
+}
